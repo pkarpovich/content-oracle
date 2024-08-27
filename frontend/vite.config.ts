151453ab42785f9fb03c0f5170323dee3abc62c9
Pavel Karpovich <pavel.karpovich@outlook.com>
2024-08-27 19:58:55 +0200

    feat(frontend): add vite-plugin-pwa for PWA support and configure manifest

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,8 +2,28 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), TanStackRouterVite(), svgr({ include: "**/*.svg" })],
+    plugins: [
+        react(),
+        TanStackRouterVite(),
+        svgr({ include: "**/*.svg" }),
+        VitePWA({
+            registerType: "autoUpdate",
+            manifest: {
+                name: "Content Oracle",
+                short_name: "Content Oracle",
+                theme_color: "#2c2c2e",
+                background_color: "#2c2c2e",
+                icons: [
+                    {
+                        type: "image/icon",
+                        src: "/favicon.ico",
+                    },
+                ],
+            },
+        }),
+    ],
 });
